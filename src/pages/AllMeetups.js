59af3d0fe72863016738012acf5f8b45606fd714
Souhail Hoichtia <souhailsrp@gmail.com>
2021-11-27 00:53:33 +0100

    spliting meetuplist, rendering all meetup, adding the meetup item and meetup list

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import MeetupList from "../components/meetups/MeetupList";
 const DUMMY_DATA = [
   {
     id: "m1",
@@ -15,7 +16,7 @@ const DUMMY_DATA = [
       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
     address: "Meetupstreet 5, 12345 Meetup City",
     description:
-      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+      "This is a secc, amazing meetup which you definitely should not miss. It will be a lot of fun!",
   },
 ];
 
@@ -23,11 +24,7 @@ function AllMeetupspage() {
   return (
     <section>
       <h1>All meetups</h1>
-      <ul>
-        {DUMMY_DATA.map((meetup) => {
-          return <li key={meetup.id}>{meetup.title}</li>;
-        })}
-      </ul>
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
