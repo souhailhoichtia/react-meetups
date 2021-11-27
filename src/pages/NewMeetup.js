cfb6b0a306aca7867514aeecd565e1e560d01e22
Souhail Hoichtia <souhailsrp@gmail.com>
2021-11-28 00:57:44 +0100

    submitting data to firebase host, using JS method fetch

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,6 +3,14 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetupPage() {
   function addMeetupHandler(meetupData) {
     // ici send http request
+    // // https://meetup-eddaa-default-rtdb.firebaseio.com/
+    fetch("https://meetup-eddaa-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "content-type": "applocation/json",
+      },
+    });
   }
   return (
     <section>
